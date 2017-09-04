f9cd94d5daa8ff6d62634eb6fa41b9723afaf7ac
gali7221 <gali7221@gmail.com>
2017-09-04 00:16:36 -0400

    Play with Reacts composition philosophy

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
+class ContactList extends React.Component {
+  render() {
+    const people = this.props.contacts
+
+    return <ol>
+      {people.map(person => (
+        <li key={person.name}>{person.name}</li>
+      ))}
+    </ol>
+  }
+}
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className = 'App' >
+        <ContactList contacts={[
+          {name:'Michael'},
+          {name:'Tyler'},
+          {name:'Robert'}
+        ]}/>
+        <ContactList contacts={[
+          {name:'Michael'},
+          {name:'Tyler'},
+          {name:'Robert'}
+        ]}/>
       </div>
     );
   }
