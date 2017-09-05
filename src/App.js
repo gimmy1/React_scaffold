63a5193cc135040afc2988dd154fa50e77881745
gali7221 <gali7221@gmail.com>
2017-09-05 19:02:48 -0400

    Update search toolbar with state management

--- src/App.js
+++ src/App.js
@@ -1,33 +1,45 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import ListContacts from './ListContacts'
 
-class ContactList extends React.Component {
-  render() {
-    const people = this.props.contacts
-
-    return <ol>
-      {people.map(person => (
-        <li key={person.name}>{person.name}</li>
-      ))}
-    </ol>
-  }
-}
 class App extends Component {
+  state = {
+    contacts: [
+      {
+        "id": "ryan",
+        "name": "Ryan Florence",
+        "email": "ryan@reacttraining.com",
+        "avatarURL": "http://localhost:5001/ryan.jpg"
+      },
+      {
+        "id": "michael",
+        "name": "Michael Jackson",
+        "email": "michael@reacttraining.com",
+        "avatarURL": "http://localhost:5001/michael.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "email": "tyler@reacttraining.com",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+    ]
+  }
+
+  removeContact = (contact) => {
+    this.setState((state) => ({
+      contacts: state.contacts.filter((c) => c.id !== contact.id)
+    }))
+  }
   render() {
     return (
-      <div className = 'App' >
-        <ContactList contacts={[
-          {name:'Michael'},
-          {name:'Tyler'},
-          {name:'Robert'}
-        ]}/>
-        <ContactList contacts={[
-          {name:'Michael'},
-          {name:'Tyler'},
-          {name:'Robert'}
-        ]}/>
+      <div>
+      <ListContacts
+        onDeleteContact={this.removeContact}
+        contacts={this.state.contacts}/>
       </div>
-    );
+    )
   }
 }
+
 
 export default App;
